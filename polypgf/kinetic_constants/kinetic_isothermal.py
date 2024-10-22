d8b8a58aea2ab103261b3137f4247c069e18a9d2
Nicolas <nicolas@PC-casa.fibertel.com.ar>
2024-10-22 17:08:10 -0300

    agrego la constante cinetica a una temperatura dadad

---
+++ polypgf/kinetic_constants/kinetic_isothermal.py
@@ -0,0 +1,16 @@
+import numpy as np
+class KineticIsothermal:
+    def __init__(self, k, T, name):
+        self.k = k
+        self.T = T
+        self.name = name
+
+    def __repr__(self):
+        return (f"Kinetic Constant Isothermal\n"
+                f"{'-'*30}\n"
+                f"name = {self.name}\n"
+                f"k  = {self.k}\n"
+                f"T = {self.T} K\n")
+    
+    def value(self):
+        return self.k
