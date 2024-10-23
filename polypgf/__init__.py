ab8aedb7100690955fe171187d68d021840fd588
Nico <a.nicolah@gmail.com>
2024-10-23 14:48:11 -0300

    guardo los test

---
+++ polypgf/__init__.py
@@ -0,0 +1,3 @@
+from . import energy_balances, inversions, kinetic_constants, models, thermo
+
+__all__ = ["energy_balances", "inversions", "kinetic_constants", "models", "thermo"]
---
+++ polypgf/kinetic_constants/__init__.py
@@ -0,0 +1,3 @@
+from polypgf.kinetic_constants.kinetic_arrhenius import KineticArrhenius
+
+__all__ = ["KineticArrhenius"]
