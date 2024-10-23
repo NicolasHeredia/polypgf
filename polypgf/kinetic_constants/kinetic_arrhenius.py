03f948675b83168ba2585d6a442b8affaa1021e3
Nico <a.nicolah@gmail.com>
2024-10-23 12:05:34 -0300

    guardo que esto esta bien

--- polypgf/kinetic_constants/kinetic_arrhenius.py
+++ polypgf/kinetic_constants/kinetic_arrhenius.py
@@ -1,21 +1,40 @@
 import numpy as np
+from typing import Union, Optional
+
 class KineticArrhenius:
-    def __init__(self, A, Ea, name, R=8.314):
-        self.A = A
-        self.Ea = Ea
-        self.name = name
-        self.R = R
+    def __init__(self, *,
+                 name: str,
+                 Ea: Union[int, float],
+                 A: Optional[Union[int, float]] = None,
+                 k_ref: Optional[Union[int, float]] = None,
+                 T_ref: Optional[Union[int, float]] = None,
+                 R: float = 8.314):
+        
+        self.name: str = name
+        self.Ea: Union[int, float] = Ea
+        self.A: Optional[Union[int, float]] = A
+        self.k_ref: Optional[Union[int, float]] = k_ref
+        self.T_ref: Optional[Union[int, float]] = T_ref
+        self.R: float = R
 
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (f"Kinetic Constant Arrhenius\n"
                 f"{'-'*30}\n"
                 f"name = {self.name}\n"
                 f"A  = {self.A}\n"
                 f"Ea = {self.Ea} J/mol\n"
                 f"R  = {self.R} J/mol·K\n")
-    
-    def value(self,  T):
-        return self.A * np.exp(-self.Ea / (self.R * T))
 
-    
+    def value(self, T: Union[int, float]) -> float:
+        if self.A is not None:
+            return self.A * np.exp(-self.Ea / (self.R * T))
+        elif self.k_ref is not None and self.T_ref is not None:
+            return self.k_ref * np.exp(- (self.Ea / self.R) * ((1 / self.T_ref) - (1 / T)))
+        else:
+            raise ValueError("Either 'A' or both 'k_ref' and 'T_ref' must be provided.")
+
+
+
+kk = KineticArrhenius(A=200, name='propagation', Ea=2.5)
+print(kk)
+print(kk.value(350))
