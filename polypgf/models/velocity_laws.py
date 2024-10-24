cc7d59c082725071664ebee3c2a20543c0c8cae6
Nico <a.nicolah@gmail.com>
2024-10-24 14:21:13 -0300

    guardo para seguir en mi casa

--- polypgf/models/velocity_laws.py
+++ polypgf/models/velocity_laws.py
@@ -1,4 +1,3 @@
-"""
 import numpy as np
 from polypgf.kinetic_constants.kinetic_arrhenius import KineticArrhenius
 from polypgf.thermo.ideal_solution import IdealSolution
@@ -6,7 +5,7 @@ from dataclasses import dataclass
 from typing import float64, Union
 
 @dataclass
-class Reactions:
+class ReactionType:
 
     descomposition = 'descomposition'
     initiation = 'initiation'
@@ -16,4 +15,49 @@ class Reactions:
     monomer_transfer = 'monomer_transfer'
     solvent_transfer = 'solvent_transfer'
 
-"""
+    def _moles(N, V):
+        return N / V
+
+REACTION_SPECS = {
+    ReactionType.descomposition: {
+        'reactants': {'I': 1},
+        'products': {},
+        'rate_equation': lambda k, conc: k * conc['A'] * conc['O2']
+    },
+    ReactionType.initiation: {
+        'reactants': {'A': 1, 'H2O': 1},
+        'products': {'B': 1, 'C': 1},
+        'rate_equation': lambda k, conc: k * conc['A'] * conc['H2O'],
+        'delta_H': -50.0
+    },
+    ReactionType.propagation: {
+        'reactants': {'A': 1},
+        'products': {'B': 1},
+        'rate_equation': lambda k, conc: k * conc['A'],
+        'delta_H': 30.0
+    },
+    ReactionType.termination_comb: {
+        'reactants': {'A': 1},
+        'products': {'B': 1, 'C': 1},
+        'rate_equation': lambda k, conc: k * conc['A'],
+        'delta_H': 45.0
+    },
+    ReactionType.termination_desp: {
+        'reactants': {'A': 1, 'B': 1},
+        'products': {'C': 1},
+        'rate_equation': lambda k, conc: k * conc['A'] * conc['B'],
+        'delta_H': -85.0
+    },
+    ReactionType.monomer_transfer: {
+        'reactants': {'A': 1, 'B': 1},
+        'products': {'C': 1},
+        'rate_equation': lambda k, conc: k * conc['A'] * conc['B'],
+        'delta_H': -85.0
+    },
+    ReactionType.solvent_transfer: {
+        'reactants': {'A': 1, 'B': 1},
+        'products': {'C': 1},
+        'rate_equation': lambda k, conc: k * conc['A'] * conc['B'],
+        'delta_H': -85.0
+    }
+}
